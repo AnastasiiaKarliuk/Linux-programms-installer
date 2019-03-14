fdb760c0cfbf4f3d80ef878599d86f0806f8435b
Anastasia <karliukanastasia@gmail.com>
2019-03-14 09:00:13 +0200

    added python-libraries for images

--- script.bash
+++ script.bash
@@ -1,15 +1,31 @@
 #!/bin/bash
 echo "Hello, honey"
-
 sudo apt-get update
+sudo apt-get install git
+git config --global user.name "FIRST_NAME LAST_NAME"
+git config --global user.email "MY_NAME@example.com"
+git config credential.helper store
+printf "\n\n\n\n\nGit installed\n"
+git --version
+printf "####################\n\n\n\n\n\n\n"
+sudo apt install snapd snapd-xdg-open
+sudo snap install pycharm-community --classic
+printf "\n\n\n\n\nPyCharm installed\n"
+printf "####################\n\n\n\n\n\n\n"
+sudo snap install telegram-desktop
+printf "\n\n\n\n\nTelegram installed\n"
+printf "####################\n\n\n\n\n\n\n"
+sudo snap install slack --classic
+printf "\n\n\n\n\nSlack installed\n"
+printf "####################\n\n\n\n\n\n\n"
 sudo apt-get install python3.6
-echo "Python installed"
+printf "\n\n\n\n\nPython installed\n"
 python3 --version
-
+printf "####################\n\n\n\n\n\n\n"
 sudo apt-get install python3-pip
-echo "pip installed"
-pip3 --version
-
+printf "\n\n\n\n\npip installed"
+pip --version
+printf "####################\n\n\n\n\n\n\n"
 pip3 install -U numpy
 pip3 install pandas
 pip3 install tensorflow
@@ -18,11 +34,22 @@ pip3 install matplotlib
 pip3 install nltk
 pip3 install xlrd
 pip3 install -U spacy
+python3 -m spacy download en
 pip3 install -U scikit-learn
 pip3 install flask
 pip3 install bs4
+pip3 install simplejson
+pip3 install facebook_business
 pip3 install pickle-mixin
-python3 -m spacy download en
-echo "DONE. HAVE A GOOD DAY WITH PYTHON"
-
+pip3 install spotipy
+pip3 install html2json
+pip3 install python-tk
+pip3 install pyqt5
+pip3 install plotly
+pip3 install requests
+pip3 install opencv-python
+sudo apt-get install tesseract-ocr
+sudo apt-get install python3-pypdf2
+printf "\n\n\n\n\n\n\n\nDONE. HAVE A GOOD DAY )\n"
+printf "####################\n"
 
